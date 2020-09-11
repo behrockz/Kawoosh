54c691a29f78e94892dd1533b6bb897490965760
Pierre Sauvage <m.sauvage.pierre@gmail.com>
2020-09-11 22:48:03 +0200

    wip

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ export ASTRA_CLUSTER_REGION=us-east1
 export ASTRA_DB_NS=kawoosh
 export ASTRA_DB_USERNAME=kawoosh
 export ASTRA_DB_PASSWORD=kawoosh
-export ASTRA_AUTHORIZATION_TOKEN=e8170076-96c6-472f-86ef-c3924c599e7d
+export ASTRA_AUTHORIZATION_TOKEN=9ecb22a6-199f-4427-ab38-730834790865
 
 # BASH Is life
 
@@ -21,6 +21,15 @@ export ASTRA_AUTHORIZATION_TOKEN=e8170076-96c6-472f-86ef-c3924c599e7d
 
 #curl --request PUT --url "https://${ASTRA_CLUSTER_ID}-${ASTRA_CLUSTER_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${ASTRA_DB_NS}/collections/faaa/blabliblou" --header 'accept: */*' --header 'content-type: application/json' --header "x-cassandra-request-id: `uuidgen`" --header "x-cassandra-token: ${ASTRA_AUTHORIZATION_TOKEN}" --data '{ "id": "BISOUS", "uuid":"4aa39c79-2439-46b6-87f4-6b1b55841ad4", "documentId": "my-first-post-a6h54","title": "Hello World","author": {"name": "Cliff Wicklow"}}'
 
-curl --request GET --url https://${ASTRA_CLUSTER_ID}-${ASTRA_CLUSTER_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${ASTRA_DB_NS}/collections/faaa/blabliblou --header 'accept: */*' --header 'content-type: application/json' --header "x-cassandra-request-id: `uuidgen`" --header "x-cassandra-token: ${ASTRA_AUTHORIZATION_TOKEN}"
+
+
 
 #curl --request PATCH --url https://${ASTRA_CLUSTER_ID}-${ASTRA_CLUSTER_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${ASTRA_DB_NS}/collections/faaa/blabliblou --header 'accept: */*' --header 'content-type: application/json' --header "x-cassandra-request-id: `uuidgen`" --header "x-cassandra-token: ${ASTRA_AUTHORIZATION_TOKEN}" --data '{"author2": {"name": "Cliff Wicklow"}}'
+
+
+curl -ik --request PATCH --url "https://${ASTRA_CLUSTER_ID}-${ASTRA_CLUSTER_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${ASTRA_DB_NS}/collections/2020_QUA1_PLA1_GRP1_CL1/config1" --header 'accept: */*' --header 'content-type: application/json' --header "x-cassandra-request-id: `uuidgen`" --header "x-cassandra-token: ${ASTRA_AUTHORIZATION_TOKEN}" --data '{"IP1":{"year":"2020","quarter":"QUA1","platform":"PLA1","group":"GRP1","clusterName":"CL1","nodeIp":"IP1","filename":"name1","confName":"config1","value":"val1","id":"config1","propertyName":"IP1","collectionName":"2020-QUA1-PLA1-GRP1-CL1"}}'
+
+curl -ik --request PATCH --url "https://${ASTRA_CLUSTER_ID}-${ASTRA_CLUSTER_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${ASTRA_DB_NS}/collections/2020_QUA1_PLA1_GRP1_CL1/config1" --header 'accept: */*' --header 'content-type: application/json' --header "x-cassandra-request-id: `uuidgen`" --header "x-cassandra-token: ${ASTRA_AUTHORIZATION_TOKEN}" --data '{"IP2":{"year":"2020","quarter":"QUA1","platform":"PLA1","group":"GRP1","clusterName":"CL1","nodeIp":"IP1","filename":"name1","confName":"config1","value":"val1","id":"config1","propertyName":"IP1","collectionName":"2020-QUA1-PLA1-GRP1-CL1"}}'
+
+
+curl --request GET --url https://${ASTRA_CLUSTER_ID}-${ASTRA_CLUSTER_REGION}.apps.astra.datastax.com/api/rest/v2/namespaces/${ASTRA_DB_NS}/collections/2020_QUA1_PLA1_GRP1_CL1/config1 --header 'accept: */*' --header 'content-type: application/json' --header "x-cassandra-request-id: `uuidgen`" --header "x-cassandra-token: ${ASTRA_AUTHORIZATION_TOKEN}"
