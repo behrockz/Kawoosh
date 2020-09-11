da42e6c7cc259722eda6da1b2821c158b45291f3
Jeremy <jeremy.artero@gmail.com>
2020-09-11 17:16:55 +0200

    add basic unit test for yaml reader

--- src/test/java/com/datastax/kawoosh/parser/fileReader/ReaderTest.java
+++ src/test/java/com/datastax/kawoosh/parser/fileReader/ReaderTest.java
@@ -8,19 +8,34 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ReaderTest {
+class ReaderTest {
     @Test
-    public void readCfStats() {
+    void readTableStats() {
         Reader reader = new TableStatReader();
         Stream<Pair> output = reader.read("src/test/resources/cfstats");
+
         assertNotNull(output);
+
         List<String> keys = output
                 .map(f -> f.getKey())
                 .collect(Collectors.toList());
 
         assertTrue(keys.contains("system_distributed.parent_repair_history.Average tombstones per slice (last five minutes)"));
         assertTrue(keys.contains("system.range_xfers.SSTable count"));
-        assertTrue(keys.contains("activity_persister.household_activities.Compacted partition mean bytes"));
+        assertTrue(keys.contains("detonation_persister.household_activities.Compacted partition mean bytes"));
+    }
+
+    @Test
+    void readYaml() {
+        Reader reader = new YamlReader();
+        Stream<Pair> output = reader.read("src/test/resources/cassandra.yaml");
+
+        List<String> keys = output
+                .map(f -> f.getKey())
+                .collect(Collectors.toList());
+
+        assertTrue(keys.contains("dynamic_snitch_reset_interval_in_ms"));
+        assertTrue(keys.contains("user_defined_function_fail_heap_mb"));
     }
 
     @Test
